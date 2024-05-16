14d4620076d846508a7149107d24d47842bf96ea
pengbo <pbstar@sina.cn>
2024-05-16 18:23:42 +0800

    docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "P-Seamless-Scroll",
+  description: "P-Seamless-Scroll 是一个创建无缝滚动效果的 js 插件。它有着轻量且高效的特性，支持丰富的自定义配置选项，提供了一系列 API 方法以及事件监听功能。",
+  lastUpdated: true,
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '文档', link: '/docs/introduce' },
+      { text: '在线体验', link: '/demo/index' }
+    ],
+
+    sidebar: {
+      '/docs/': [
+        {
+          // text: '官方文档',
+          items: [
+            { text: '介绍', link: '/docs/introduce' },
+            { text: '开始', link: '/docs/start' },
+            { text: '配置', link: '/docs/config' },
+            { text: '属性', link: '/docs/stats' },
+            { text: '方法', link: '/docs/method' },
+            { text: '事件', link: '/docs/event' }
+          ]
+        }
+      ]
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/pbstar/p-seamless-scroll' }
+    ],
+    editLink: {
+      pattern: 'https://github.com/pbstar/p-seamless-scroll/edit/main/docs/:path'
+    }
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "P-Seamless-Scroll"
+  tagline: 一个创建无缝滚动效果的 js 插件。它有着轻量且高效的特性，支持丰富的自定义配置选项，提供了一系列 API 方法以及事件监听功能。
+  image:
+    src: ./imgs/logo.jpg
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: 快速上手
+      link: /markdown-examples
+    - theme: alt
+      text: 在线体验
+      link: /api-examples
+
+features:
+  - title: 轻量
+    details: 轻巧简洁，不依赖任何外部插件，插件大小仅6k，专注于核心滚动功能，减少页面负担。
+  - title: 高效
+    details: 提供流畅、稳定的滚动效果，支持自定义配置，满足各种不同场景的需求。
+  - title: 开源
+    details: 开源项目，可免费使用、修改和分享，促进社区合作与发展。
+---
+
