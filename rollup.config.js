5e06e2620b51d6f372b1f0cfae4b9583e90624c1
pengbo <pbstar@sina.cn>
2024-05-24 17:24:30 +0800

    rollup banner

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,24 @@
 import terser from "@rollup/plugin-terser";
+import pa from "./package.json" assert { type: 'json' }; 
+const banner = `/*!  
+* p-seamless-scroll v${pa.version}
+* Copyright 2024 Pbstar (https://github.com/pbstar)
+* Licensed under MIT (https://github.com/pbstar/p-seamless-scroll/blob/main/LICENSE)
+*/
+`
 export default {
   input: "src/index.js",
   output: [
     {
       file: "lib/p-seamless-scroll.es.js",
-      format: "es"
+      format: "es",
+      banner
     },
     {
       file: "lib/p-seamless-scroll.umd.js",
       format: "umd",
-      name: 'pSeamlessScroll'
+      name: 'pSeamlessScroll',
+      banner
     }
   ],
   plugins: [
